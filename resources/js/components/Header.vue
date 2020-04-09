159578ff30b99b1a7cd706e016fe285f1146dc7e
widayat <widayat@umroh.com>
2020-04-10 06:39:08 +0700

    first_commit

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,339 @@
+<template>
+    <div>
+        <div class="container">
+            <div>
+
+            </div>
+            <!-- end header -->
+            <div class="row">
+                <div class="col-md-6">
+                    <h2><b>PHP Hosting</b></h2>
+                    <h2>Cepat, Handal, penuh dengan module PHP yang Anda Butuhkan</h2>
+                    <ul>
+                        <li>Solusi PHP untuk performa query yang lebih cepat</li>
+                        <li>Konsumsi memori yang lebih rendah</li>
+                        <li>Suport PHP 5.3, PHP 5.4, PHP 5.5, PHP 5.6, PHP 7</li>
+                        <li>Fitur enkripsi IonCube dan Zend Guard Loaders</li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <img src="images/svg/illustration banner PHP hosting-01.svg" alt="">
+                </div>
+            </div>
+            <!-- end bunner -->
+            <div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="" alt="">
+                        <p>PHP Zend Guard Loader</p>
+                    </div>   
+                    <div class="col-sm-4">
+                        <img src="" alt="">
+                        <p>PHP Composer</p>
+                    </div>   
+                    <div class="col-sm-4">
+                        <img src="" alt="">
+                        <p>PHP ionCube Loader</p>
+                    </div>                
+                </div>
+            </div>
+            <!-- end top-body -->
+            <div>
+                <h2 class="text-center"><b>Paket Hosting Singapura yang Tepat</b></h2>
+                <h4>Diskon 40% + Domain dan SSl Gratis untuk Anda</h4>
+
+            </div>
+            <!-- end body list-price -->
+            <div>
+                <h2 class="text-center">Powerful dengan limit PHP yang Lebih Besar</h2>
+                <div></div>
+            </div>
+            <!-- end pody disc -->
+            <div>
+                <h2 class="text-center">Semua Paket Hosting Sudah Termasuk</h2>
+                <div class="row text-center">
+                <div class="col-sm-4 p-5">
+                <div class="service">
+                    <img src="images/svg/icon PHP Hosting_PHP Semua Versi.svg" alt="keuntungan-icon" class="img-circle">
+                </div>
+                <h4>PHP Semua Versi</h4>
+                <p>Pilih mulai dari versi PHP 5.3 s/d PHP 7. Ubah sesuka Anda</p>
+                </div>
+                <div class="col-sm-4 p-5">
+                <div class="service">
+                    <img src="images/svg/icon PHP Hosting_My SQL.svg" alt="call-icon" class="img-circle">
+                </div>
+                <h4>Mysql Versi 5.6</h4>
+                <p>Nikmati MySQL versi terbaru, tercepat dan kaya akan fitur.</p>
+                </div>
+                <div class="col-sm-4 p-5">
+                <div class="service">
+                    <img class="img-circle" src="images/svg/icon PHP Hosting_CPanel.svg" alt="daftar-icon">
+                </div>
+                <h4>Panel Hosting cPanel</h4>
+                <p>Kelola website dengan panel canggih yang familiar di hati Anda.</p>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-sm-4 p-5">
+                <div class="service">
+                    <img src="images/svg/icon PHP Hosting_garansi uptime.svg" alt="keuntungan-icon" class="img-circle">
+                </div>
+                <h4>Garansi Uptime 99.9%</h4>
+                <p>Data center yang mendukung kelangsungan website anda 24/7.</p>
+                </div>
+                <div class="col-sm-4 p-5">
+                <div class="service">
+                    <img src="images/svg/icon PHP Hosting_InnoDB.svg" alt="call-icon" class="img-circle">
+                </div>
+                <h4>Database InnoDB Unlimited</h4>
+                <p>Jumlah dan ukuran database yang dibutuhkan sesuai kebutuhan Anda.</p>
+                </div>
+                <div class="col-sm-4 p-5">
+                <div class="service">
+                    <img class="img-circle" src="images/svg/icon PHP Hosting_My SQL remote.svg" alt="daftar-icon">
+                </div>
+                <h4>Wildcard Remote MYSQL</h4>
+                <p>Mendukung s/d 25 max_user_connections dan 100 max_connections.</p>
+                </div>
+            </div>
+            </div>
+            <!-- end list body hosting -->
+            <div>
+                <h2 class="text-center">Mendukung Penuh Framework Laravel</h2>
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>Tak perlu menggunakan dedicated VPS yang mahal. Layanan PHP Hosting murah kami mendukung framework favorite Anda</p>
+                        <ul>
+                            <li>Install Laravel <b>1 Klik</b> dengan softaculus Installer.</li>
+                            <li>Mendukung extensi <b>PHP MyCrypt, phar, mbstring, json,</b> dan <b>fileinfo</b></li>
+                            <li>Tersedia <b>Composer</b> dan <b>SSH</b> untuk menginstall package pilihan Anda</li>
+                        </ul>
+                        <span>Nb. Composer dan SSH hanya tersedia pada paket personal dan bisnis</span>
+                        <a href="#">Pilih Hosting Anda</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/svg/illustration banner support laravel hosting.svg" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- end body support -->
+            <div>
+                <h2 class="text-center">Module Lengkap untuk Menjalankan Aplikasi  PHP Anda</h2>
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-lg-3">
+                        <ul>
+                            <li>IcePHP</li>
+                            <li>apc</li>
+                            <li>apcu</li>
+                            <li>apm</li>
+                            <li>ares</li>
+                            <li>bcmath</li>
+                            <li>bcompiler</li>
+                            <li>big_int</li>
+                            <li>bitset</li>
+                            <li>blommy</li>
+                            <li>bz2_filter</li>
+                            <li>clamav</li>
+                            <li>coin_acceptor</li>
+                            <li>crack</li>
+                            <li>dba</li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-6 col-lg-3">
+                        <ul>
+                            <li>http</li>
+                            <li>huffman</li>
+                            <li>idn</li>
+                            <li>igbinary</li>
+                            <li>imagick</li>
+                            <li>imap</li>
+                            <li>inclued</li>
+                            <li>inotify</li>
+                            <li>interbase</li>
+                            <li>intl</li>
+                            <li>ioncube_loader</li>
+                            <li>ioncube_loader_4</li>
+                            <li>jsmin</li>
+                            <li>json</li>
+                            <li>idap</li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-6 col-lg-3">
+                        <ul>
+                            <li>nd_pdo_mysql</li>
+                            <li>oauth</li>
+                            <li>oci8</li>
+                            <li>odbc</li>
+                            <li>opcache</li>
+                            <li>pdf</li>
+                            <li>pdo</li>
+                            <li>pdo_dblib</li>
+                            <li>pdo_firebird</li>
+                            <li>pdo_mysql</li>
+                            <li>pdo_odbc</li>
+                            <li>pdo_pgsql</li>
+                            <li>pdo_sqlite</li>
+                            <li>pgsql</li>
+                            <li>phalcon</li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-sm-6 col-lg-3">
+                        <ul>
+                            <li>stats</li>
+                            <li>stem</li>
+                            <li>stomp</li>
+                            <li>suhosin</li>
+                            <li>sybase-ct</li>
+                            <li>sysvmsg</li>
+                            <li>sysvem</li>
+                            <li>sysvshm</li>
+                            <li>tidy</li>
+                            <li>timezondb</li>
+                            <li>trader</li>
+                            <li>translit</li>
+                            <li>uploadprogress</li>
+                            <li>uri_template</li>
+                            <li>uuid</li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <a href="#">Selengkapnya</a>
+                </div>
+            </div>
+            <!-- end body module -->
+
+            <div>
+                <h2>Linux Hosting yang Stabil dengan Teknologi LIVE</h2>
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>SuperMicro Intel Xeon 24-Cores server dengan RAM <b>128 GB</b> dan Teknologi <b>LIVE CloundLinux</b> untuk stabilitas server Anda. Dilengkapi dengan <b>SSD</b> untuk kecepatan <b>MYSQL</b> dan chaching. Apache load balancer berbasis LiteSpeed Technologies, <b>CageFS</b> security, <b>Raid-10</b> protection dan auto backup untuk keamanan PHP Anda.</p>
+                        <a href="#">Pilih Hosting Anda</a>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/Image support.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Bagikan jika Anda menyukai halaman ini</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- end body-down -->
+        <div class="container">
+            <h2>Perlu <b>BANTUAN?</b> Hubungi kami : <b>0274-5305505</b></h2>
+            <div>
+                <a href="#"><i></i>Live Chat</a>
+            </div>
+        </div>
+
+        <!-- footer -->
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <h5>HUBUNGI KAMI</h5>
+                    <p>0274-5305505 <br>
+                    Senin - Minggu <br>
+                    24 Jam Nonstop</p>
+                    <p>Jl.Selokan Mataram Monjali <br>
+                    Karangjati MT I/304 <br>
+                    Sinduadi, Mlati, Sleman <br>
+                    Yogyakarta 55284</p>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <h5>LAYANAN</h5>
+                    <ul>
+                        <li>Domain</li>
+                        <li>Shared Hosting</li>
+                        <li>Cloud VPS Hosting</li>
+                        <li>Managed VPS Hosting</li>
+                        <li>Web Builder</li>
+                        <li>Keamanan SSL/HTTPS</li>
+                        <li>Jasa Pembuatan Website</li>
+                        <li>Program Aftifiliansi</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <h5>SERVICE HOSTING</h5>
+                    <ul>
+                        <li>Hosting Murah</li>
+                        <li>Hosting Indonesia</li>
+                        <li>Hosting Singapura SG</li>
+                        <li>Hosting PHP</li>
+                        <li>Hosting Wordpress</li>
+                        <li>Hosting Laravel</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <h5>TUTORIAL</h5>
+                    <ul>
+                        <li>Knowledgebase</li>
+                        <li>Blog</li>
+                        <li>Cara Pembayaran</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <h5>TENTANG KAMI</h5>
+                    <ul>
+                        <li>Tim Niagahoster</li>
+                        <li>Karir</li>
+                        <li>Events</li>
+                        <li>Penawaran & Promo Spesial</li>
+                        <li>Kontak Kami</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <h5>KENAPA MEMILIH NIAGAHOSTER?</h5>
+                    <ul>
+                        <li>Support Terbaik</li>
+                        <li>Garansi Harga Murah</li>
+                        <li>Domain Gratis Selamanya</li>
+                        <li>Datacenter Hosting Terbaik</li>
+                        <li>Review Pelanggan</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <h5>NEWSLETTER</h5>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3">
+
+                </div>
+            </div>
+
+            <div>
+                <h5>PEMBAYARAN</h5>
+                <p>Aktivasi instandengan e-payment. Hosting dan domain langsung aktif!</p>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-md-7">
+                    <p>Copyright ©2016 Niagahoster | Hosting powered by PHP7, CloudLinux, CloudFlare, BitNinja and DC Biznet Technologie Jakarta</p>
+                    <p>VPS Murah powered by Webuzo Softaculous, Intel SSD and cloud computing technology</p>
+                </div>
+                <div class="col-12 col-md-5 mt-5 mt-md-0">
+                    <p>Syarat dan Ketentuan | Kebijakan Privasi</p>
+                </div>
+            </div>
+        </div>
+    </div>  
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
